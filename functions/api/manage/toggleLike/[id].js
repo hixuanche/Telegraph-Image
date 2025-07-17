ed91896acf3190b015d0b43776e764351a4a65e8
玄澈 • XuanChe <217778588+hixuanche@users.noreply.github.com>
2025-07-17 16:58:52 +0800

    Update [id].js

--- functions/api/manage/toggleLike/[id].js
+++ functions/api/manage/toggleLike/[id].js
@@ -3,14 +3,14 @@ export async function onRequest(context) {
 
     console.log("Request ID:", params.id);
 
-    // 获取元数据
+    // 獲取元數據
     const value = await env.img_url.getWithMetadata(params.id);
     console.log("Current metadata:", value);
 
-    // 如果记录不存在
+    // 如果記錄不存在
     if (!value.metadata) return new Response(`Image metadata not found for ID: ${params.id}`, { status: 404 });
 
-    // 切换 liked 状态并更新
+    // 切換 liked 狀態並更新
     value.metadata.liked = !value.metadata.liked;
     await env.img_url.put(params.id, "", { metadata: value.metadata });
 
